ca6f92eb67bd6018814af9cfe4f1d96d05b5647d
UselessFlower <denisbar332@gmail.com>
2024-02-05 19:07:05 +0300

    Newest work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import * as PIXI from "pixi.js";
+/* import {Loader} from 'pixi.js' */
 import { gsap } from "gsap";
 import ToysFirst from '../spritesTWO/ToysFirst.json'
+import BallsImport from '../spritesTWO/balls/Balls.json'
 import { Assets, Sprite } from 'pixi.js';
 import {DropShadowFilter} from '@pixi/filter-drop-shadow';
 
     await PIXI.Assets.load('../spritesTWO/ToysFirst.json')
     await PIXI.Assets.load('../spritesTWO/ButtonsAndMore.json')
     await PIXI.Assets.load('../spritesTWO/Books.json')
+    /* const balls = await PIXI.Assets.load('../spritesTWO/balls/Balls.json') */
 
     const app = new PIXI.Application({background: 'blue', width:632, height:478, view: document.getElementById('MyCanvas')});
     /* document.body.appendChild(app.view); */
@@ -37,7 +40,7 @@ import {DropShadowFilter} from '@pixi/filter-drop-shadow';
 
    let toys = []
    let frameNames = Object.keys(ToysFirst.frames);
-   console.log(frameNames)
+   console.log(`asd ${frameNames}`)
 
    function RandomNumberOfToys(i){
     for(let x = 0; x <i;){
@@ -68,37 +71,36 @@ import {DropShadowFilter} from '@pixi/filter-drop-shadow';
    container.addChild(ToyButtonContainer);
    function Game(){
     if (scene == 1){
-        /* poryadok.splice(poryadok.length) */
         rand(5)
-        RandomNumberOfToys(3)
-        for (let i =0; i < 3; i++){ drowToys(toys[i], 0.60,poryadokOfTheToysOnTheShelf(poryadok[i]), toysContainer)}
-        drowBooks()
+        RandomNumberOfToys(6)
+        for (let i =0; i < 3; i++){ drawToys(toys[i], 0.60,poryadokOfTheToysOnTheShelf(poryadok[i]), toysContainer)}
+        drawBooks()
         
-        drowButton('запомнил', 0xFFFF00, container, {x: container.width/150, y:200} )
+        drawButton('запомнил', 0xFFFF00, container, {x: container.width/150, y:200} )
     }
     if(scene == 2){
         
         containerOneForButton.visible= false;
-       const toyToRemove = Math.floor(Math.random()*3)
-       console.log(toyToRemove,toys,toys[toyToRemove] )
+        const toyToRemove = Math.floor(Math.random()*3)
+        console.log(toyToRemove,toys,toys[toyToRemove] )
         toysContainer.removeChildAt(toyToRemove) ; 
        
-       
-       for (let i =0; i < 2; i++){ drowToys(toys[i]+1, 1.00, poryadokAfterButton(i), ToyButtonContainer, true)}
-       drowToys(toys[toyToRemove], 1.00, poryadokAfterButton(2), ToyButtonContainer,true, true)
-      /*  ToyButtonContainer.children[i].interactive= true;
-       ToyButtonContainer.children[i].on('mouseover', ()=>{
-        ToyButtonContainer.children[i].rotation += 0.5;
-       }) */
-      
+       const randomOfthree = Math.floor(Math.random()*3)
+       console.log(`random number of the toy is ${randomOfthree}`)
+       for (let i =0; i < 3; i++){ 
+        if (i== randomOfthree){continue}
+        drawToys(toys[i+3], 1.00, poryadokAfterButton(i), ToyButtonContainer, true)
+    }
+       drawToys(toys[toyToRemove], 1.00, poryadokAfterButton(randomOfthree), ToyButtonContainer,true, true)
+   
     }
    }
 
 async function onToyClickedTrue(){
-drowUtility(12, true)
+drawUtility(12, true)
 }
 async function onToyClickedFalse(){
-    drowUtility(19, false)
+    drawUtility(19, false)
 }
 
 
@@ -106,7 +108,6 @@ async function onToyClickedFalse(){
    rows[0]= 150;
    let columns=[]
    columns[0] = 430
-   /* position[1[1]] =  */
     for(let i = 1; i<5;i++){
         for(let j = 1; j <6; j++){
             columns[i]=columns[i-1]+55
@@ -131,7 +132,7 @@ async function onToyClickedFalse(){
          bunny.on('pointerout', stopRotationOnHover)
     }
    
-   async function drowToys(spriteNumber, scale, place, father, interactive, win){
+   async function drawToys(spriteNumber, scale, place, father, interactive, win){
    
         const val =  frameNames[spriteNumber]
         const texture = PIXI.Texture.from(val) 
@@ -152,7 +153,7 @@ async function onToyClickedFalse(){
     
     
 
-    async function drowUtility(spriteNumber, win){
+    async function drawUtility(spriteNumber, win){
     const val = `3_${spriteNumber}.png`
     const texture = PIXI.Texture.from(val) 
     const bunny = new PIXI.Sprite(texture);
@@ -191,13 +192,13 @@ async function onToyClickedFalse(){
         
     }
    const containerOneForButton = new PIXI.Container();
-   function drowButton(TextToPut, color, parent, pos){
+   function drawButton(TextToPut, color, parent, pos){
     const bg = new PIXI.Sprite(PIXI.Texture.WHITE)
     bg.width = 180;
-    bg.height = 50;
-    bg.tint = color;
+    bg.height = 47;
+    bg.tint = 0xf0e47c;
     bg.anchor.set(0.5)
-    const text = new PIXI.Text(TextToPut, {fill: "black", alpha: 0.5});
+    const text = new PIXI.Text(TextToPut, {fill: 0x244bb5, alpha: 0.5});
     text.anchor.set(0.5);
      containerOneForButton.x = pos.x
     containerOneForButton.y = pos.y
@@ -208,39 +209,73 @@ async function onToyClickedFalse(){
     containerOneForButton.on('pointerover', onButtonOver)
     containerOneForButton.on('pointerout', onButtonOut);
     parent.addChild(containerOneForButton)
+
+    const textu = PIXI.Texture.from('./Sprites/2_78.png')  
+
+    const bubilda = new PIXI.Sprite(textu)
+   
+    const bubildaRight = new PIXI.Sprite(textu)
+    
+   /*  bubilda.tint = 0xf0d554 */
+    bubilda.anchor.set(0.5)
+    bubildaRight.anchor.set(0.5)
+
+    bubildaRight.angle=180
+
+    bubilda.x = bg.x-bg.width/2 -3
+    bubilda.y = bg.y
+    
+    bubildaRight.x = bg.x+bg.width/2 +3
+    bubildaRight.y = bg.y
+    containerOneForButton.addChild(bubilda)
+    containerOneForButton.addChild(bubildaRight)
    }
     function onButtonDownHide(){
         /* toysContainer.visible=false; */
+        
         scene = 2;
         Game()
+        AnimationOnButtonDown()
     }
     function onButtonOver(){
-        containerOneForButton.scale.set(0.97)
+       /*  containerOneForButton.scale.set(0.97) */
     }
     function onButtonOut(){
         containerOneForButton.scale.set(1)
     }
     
-    async function drowBooks(){
+    async function drawBooks(){
         const val1 = "2_14.png"
         const val2 = "2_35.png"
+        const val3 = "2_38.png"
+        const val4 = "2_83.png"
         poryadok.length = poryadok.length-2
         console.log(`poryadok after trim ${poryadok}`)
-        let randbook;
+        
         let val
         for (let i =1; i<7;i++){
             
             for(let j = 1; j<6; j++){
+                const random = Math.floor(Math.random()*4)
                 
-                /* randbook=Math.floor(Math.random()+1)
-                console.log(randbook)
-                let val =1;
-                if(randbook==1){
-                     val = val1
-                }if(randbook==2){
-                     val= val2
-                } */
-                i/2 == 0  ? val = val1: val=val2
+                switch (random) {
+                    case 0:
+                        val=val1
+                        break;
+                    case 1:
+                        val=val2
+                        break;
+                    case 2:
+                        val=val3
+                        break;
+                    case 3:
+                        val=val4
+                        break;
+                    default:
+                        val = val1
+                        break;
+                }
+                /* random == 0  ? val = val1: val=val2 */
                 const texture = PIXI.Texture.from(val) 
                 const bunny = new PIXI.Sprite(texture);   
                 bunny.anchor.set(0.5)
@@ -259,8 +294,88 @@ async function onToyClickedFalse(){
             }
         }
     }
+    function AnimationOnButtonDown(){
+        backk.tint = 0x999999
+        BackOnTheLeft.tint = 0x999999
+        toysContainer.visible = false
+        ToyButtonContainer.visible = false
+        setTimeout(() => {
+            backk.tint = 0xFFFFFF
+            BackOnTheLeft.tint = 0xFFFFFF
+        toysContainer.visible = true
+        ToyButtonContainer.visible = true
+        }, 1500);
+    }
+    const ContainerForBackgroundStuff = new PIXI.Container();
+    ContainerForBackgroundStuff.x = app.screen.width / 2;
+    ContainerForBackgroundStuff.y = app.screen.height / 2;
+    container.addChild(ContainerForBackgroundStuff);
+/*     function drawBalls(){
+        img = new PIXI.AnimatedSprite(balls.animation['image_se'])
+        img.anchor(0.5)
+        img.x = 0
+        img.y = 0
+        ContainerForBackgroundStuff.addChild(img)
+
+        img.play()
+        app.ticker.add(animate)
+    } */
+    /* const loader = PIXI.Loader.shared; */
+    /* loader.add('') */
+   /*  PIXI.loader.add('') */
+   await  PIXI.Assets.load('../spritesTWO/balls/Balls.json')
+   async function drawBalls(){
+       
+        
+        let frames = Object.keys(BallsImport.frames);
+        
+        const anim = new PIXI.AnimatedSprite(frames);
+        anim.x = 0
+        anim.y = 0
+        anim.anchor.set(0.5);
+        anim.animationSpeed = 0.5;
+        anim.play();
+        ContainerForBackgroundStuff.addChild(anim);
+}
+const containerForNumbers = new PIXI.Container();
+    containerForNumbers.x = container.width / 2;
+    containerForNumbers.y = container.height / 2;
+    container.addChild(containerForNumbers);
+async function drawNumbers(){
+
+    
+
+    const number1 = new PIXI.Sprite(PIXI.Texture.from('3_25.png'))
+    const number2 = new PIXI.Sprite(PIXI.Texture.from('3_26.png'))
+    const number3 = new PIXI.Sprite(PIXI.Texture.from('3_23.png'))
+    const mas = [number1,number2,number3]
+
+for(let x = 0; x<3;x++)
+{
+    
+    setTimeout(() => {
+        let a = mas[x]
+    a.x = 0+x*100
+    a.y = 0
+        container.addChild(a)
+    }, 5000*x);
+    setTimeout(() => {
+       
+        container.removeChild(mas[x]);
+    }, 10000*x+1);
+}
+
+setTimeout(() => {
+    
+}, 3000);
+
+}
+   
    Game();
    animationOnEnteringTheScene();
+   /* drawBalls(); */
+   drawNumbers()
+   
    
 
 
